4f5331ae32a13ad1c4fb225cedc6016d47df1fac
Arnab <arnabkaycee@gmail.com>
2021-02-16 00:59:37 +0530

    Add eta for rendering

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const sqlite3 = require('sqlite3');
 const config = require('./config.json');
 const util = require('util');
-
-
+const eta = require('eta');
+const path = require('path');
 // underlined -> underlined,
 // 5 - purple
 // 4 - pink
@@ -10,6 +10,18 @@ const util = require('util');
 // 2 - blue
 // 1 - green
 
+const fs = require('fs');
+const { exit } = require('process');
+let template;
+
+fs.readFile('/Users/arnab/Documents/01_Work/50_Book_Study/ibooknotes2html/layout.eta', 'utf8' , (err, data) => {
+  if (err) {
+    console.log('Unable to read template file');
+    exit(1);
+  }
+  //console.log(data);
+  template = data;
+});
 
 const NOTE_QUERY=`SELECT
 ZANNOTATIONSELECTEDTEXT as SelectedText,
@@ -53,9 +65,10 @@ const dbNote = new sqlite3.Database(config.notePath, (err) => {
   });
 
 
-function getAllBooks(){
-
-    dbBook.serialize(() => {
+async function getAllBooks() {
+    return new Promise((resolve, reject)=>{
+      let rows = [];
+      dbBook.serialize(() => {
         dbBook.each(`SELECT
                 ZASSETID,
                 ZTITLE AS Title,
@@ -63,30 +76,45 @@ function getAllBooks(){
             FROM ZBKLIBRARYASSET
             WHERE ZTITLE IS NOT NULL`, (err, row) => {
           if (err) {
-            console.error(err.message);
+            return reject("Error in fetching data");
           }
-          console.log(row);
+          rows.push(row);
         });
+        return resolve(rows);
       });
+    });
+    
 }
 
-function getAllNotes(bookId){
-    //console.log(util.format(NOTE_QUERY, bookId));
+async function getAllNotes(bookId){
+  return new Promise((resolve, reject) => {
+    let rows = [];
     dbNote.serialize(() => {
-        dbNote.each(util.format(NOTE_QUERY, bookId), (err, row) => {
-          if (err) {
-            console.error(err.message);
-          }
-          console.log(row);
-        });
+      dbNote.each(util.format(NOTE_QUERY, bookId), (err, row) => {
+        if (err) {
+          return reject("Error in fetching notes");
+        }
+        rows.push(row);
       });
+    });
+    return resolve(rows);
+  });
+  
 }
 
-function createHTML(){
-    let json = getAllNotes('4347099EFF55D26EA262175D88BA312D');
+async function createHTML(){
+    let json = await getAllNotes('4347099EFF55D26EA262175D88BA312D');
+    json = await getAllBooks()
+    // let it = {
+    //   title : "Never Split the difference",
+    //   body : [json]
+    // }
+    // eta.renderFile(path.join(__dirname, "layout.eta"), it);
+    console.log(json);
 
 }
 
-getAllNotes('4347099EFF55D26EA262175D88BA312D')
+//getAllNotes('4347099EFF55D26EA262175D88BA312D')
 //getAllBooks();
-
+//console.log(template);
+createHTML();
