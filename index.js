a822c9dab1e6bb913775f37f523b663de71d750f
Arnab <arnabkaycee@gmail.com>
2021-02-16 10:03:40 +0530

    made changes to style and rendering

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const path = require('path');
 
 const fs = require('fs');
 const { exit } = require('process');
+const { strict } = require('assert');
 let template;
 
-fs.readFile('/Users/arnab/Documents/01_Work/50_Book_Study/ibooknotes2html/layout.eta', 'utf8' , (err, data) => {
+fs.readFile('/Users/arnab/Documents/01_Work/50_Book_Study/ibooknotes2html/layout.eta', 'utf8', (err, data) => {
   if (err) {
     console.log('Unable to read template file');
     exit(1);
@@ -23,7 +24,7 @@ fs.readFile('/Users/arnab/Documents/01_Work/50_Book_Study/ibooknotes2html/layout
   template = data;
 });
 
-const NOTE_QUERY=`SELECT
+const NOTE_QUERY = `SELECT
 ZANNOTATIONSELECTEDTEXT as SelectedText,
 ZANNOTATIONNOTE as Note,
 ZFUTUREPROOFING5 as Chapter,
@@ -51,67 +52,69 @@ FROM ZBKLIBRARYASSET
 WHERE ZTITLE IS NOT NULL`
 
 const dbBook = new sqlite3.Database(config.bookPath, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the book database.');
-  });
+  if (err) {
+    console.error(err.message);
+  }
+  //console.log('Connected to the book database.');
+});
 
 const dbNote = new sqlite3.Database(config.notePath, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the note database.');
-  });
+  if (err) {
+    console.error(err.message);
+  }
+  //console.log('Connected to the note database.');
+});
 
 
 async function getAllBooks() {
-    return new Promise((resolve, reject)=>{
-      let rows = [];
-      dbBook.serialize(() => {
-        dbBook.each(`SELECT
+  return new Promise((resolve, reject) => {
+    let rows = [];
+    dbBook.serialize(() => {
+      dbBook.each(`SELECT
                 ZASSETID,
                 ZTITLE AS Title,
                 ZAUTHOR AS Author
             FROM ZBKLIBRARYASSET
             WHERE ZTITLE IS NOT NULL`, (err, row) => {
-          if (err) {
-            return reject("Error in fetching data");
-          }
-          rows.push(row);
-        });
-        return resolve(rows);
-      });
-    });
-    
-}
-
-async function getAllNotes(bookId){
-  return new Promise((resolve, reject) => {
-    let rows = [];
-    dbNote.serialize(() => {
-      dbNote.each(util.format(NOTE_QUERY, bookId), (err, row) => {
         if (err) {
-          return reject("Error in fetching notes");
+          return reject("Error in fetching data");
         }
         rows.push(row);
       });
+      return resolve(rows);
     });
-    return resolve(rows);
   });
-  
 }
 
-async function createHTML(){
-    let json = await getAllNotes('4347099EFF55D26EA262175D88BA312D');
-    json = await getAllBooks()
-    // let it = {
-    //   title : "Never Split the difference",
-    //   body : [json]
-    // }
-    // eta.renderFile(path.join(__dirname, "layout.eta"), it);
-    console.log(json);
+async function getAllNotes(bookId) {
 
+  return new Promise((resolve, reject) => {
+    const queries = [];
+    dbNote.each(util.format(NOTE_QUERY, bookId), (err, row) => {
+      if (err) {
+        reject(err); // optional: you might choose to swallow errors.
+      } else {
+        queries.push(row); // accumulate the data
+      }
+    }, (err, n) => {
+      if (err) {
+        reject(err); // optional: again, you might choose to swallow this error.
+      } else {
+        resolve(queries); // resolve the promise
+      }
+    });
+  });
+}
+
+async function createHTML() {
+  let json = await getAllNotes('4347099EFF55D26EA262175D88BA312D');
+  let it = {
+    title : "Never Split the difference",
+    data : json
+  };
+  
+  let x = await eta.renderFile(path.join(__dirname, "layout.eta"), it);
+  fs.writeFileSync('/Users/arnab/Documents/01_Work/50_Book_Study/ibooknotes2html/out.html',x);
 }
 
 //getAllNotes('4347099EFF55D26EA262175D88BA312D')
